35294149c9bbfbf06dfbe9c652757d9cde6fa575
TheClutchC <claycarney7@gmail.com>
2023-05-13 00:27:54 -0400

    build components for and import into Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import NavLinks from "./forHeader/NavLinks";
-import { Link } from "react-router-dom";
+import PhoneNumber from "./forHeader/PhoneNumber";
+import Logo from "./forHeader/Logo";
+import CompanyName from "./forHeader/CompanyName";
 
 const Header = () => {
   return (
     <div className="flex flex-row justify-stretch bg-dark-moss-green gap-2 m-2 border-2 border-solid items-center">
-      <Link to="/" className="flex-initial justify-evenly border-x-seal-brown border-y-olive border-8 font-extralight p-2">
-        LD
-      </Link>
+      <Logo />
+      <CompanyName />
       <NavLinks />
-      <div className="hidden sm:flex m-2">505-718-5499</div>
+      <PhoneNumber />
     </div>
   );
 };
---
+++ src/components/forHeader/PhoneNumber.js
@@ -0,0 +1,7 @@
+const PhoneNumber = () => {
+  return (
+    <div className="hidden sm:flex m-2">505-718-5499</div>
+  );
+};
+
+export default PhoneNumber;
