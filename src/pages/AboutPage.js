4521987e1fd4a00e217277611a9d3434910a4e9f
TheClutchC <claycarney7@gmail.com>
2023-06-04 18:35:12 -0400

    added AboutPage summary

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,59 @@
 const HomePage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      About Page
-    </h1>
-  )
-}
+    <section className="px-52 my-24">
+      <h1 className="text-3xl font-bold mb-10 text-center">
+        Our Company
+      </h1>
+      <p>
+        Established in 2022, Lamp Doctor is a trusted lamp repair company
+        known for its exceptional craftsmanship and personalized customer
+        service. We specialize in breathing new life into all kinds of lamps,
+        from novelty and antique to standard fixtures.
+      </p>
+      <br />
+      <p>
+        What sets us apart is our commitment to providing quick turnaround
+        times without compromising on quality. We understand the importance of
+        your cherished lamps, and our skilled team works diligently to restore
+        them to their former glory. Our high standards of workmanship ensure
+        that every repair is executed with precision and attention to detail.
+      </p>
+      <br />
+      <p>
+        At Lamp Doctor, we believe in the value of excellent communication. We
+        strive to keep our customers informed every step of the way, from
+        initial assessment to completion. Our prompt communication ensures
+        that you are always aware of the progress and expected timelines for
+        your lamp repair.
+      </p>
+      <br />
+      <p>
+        In addition to our repair services, we also offer light installations,
+        helping you create the perfect ambiance in your space. Whether you
+        need assistance with residential lighting or require professional
+        lighting solutions for commercial settings, we have you covered.
+      </p>
+      <br />
+      <p>
+        Our core values of honesty, integrity, and workmanship guide us in
+        every aspect of our business. We take pride in delivering transparent
+        and reliable services to our customers. You can trust us to provide
+        accurate assessments, fair pricing, and exceptional results.
+      </p>
+      <br />
+      <p>
+        Don't just take our word for it. Check out our customer reviews on
+        Yelp and hear from satisfied customers who have experienced our
+        top-notch lamp repair services firsthand.
+      </p>
+      <br />
+      <p>
+        Choose Lamp Doctor for all your lamp repair needs. With our expertise
+        and dedication, we'll restore your lamps to their original beauty and
+        functionality, bringing light back into your life.
+      </p>
+    </section>
+  );
+};
 
 export default HomePage;
