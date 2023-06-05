da649896a766cb882ba1d1ed584ca7a85dbe5ed4
TheClutchC <claycarney7@gmail.com>
2023-06-04 21:56:46 -0400

    improve layout of sitemap

--- src/components/forFooter/SiteMap.js
+++ src/components/forFooter/SiteMap.js
@@ -11,24 +11,18 @@ const SiteMap = () => {
   const linkPaths = ['/', '/about', '/services', '/contact'];
 
   return (
-    <table className="border-collapse border border-gray-800">
-      <thead>
-        <tr className="bg-gray-800 text-white text-lg font-bold">
-          <th colSpan="2">Site Map</th>
-        </tr>
-      </thead>
-      <tbody>
-        {mapLinks.map((page, index) => (
-          <tr key={page} className="border border-gray-800">
-            <td className="py-2 px-4 text-center">
-              <Link to={linkPaths[index]}>
-                {page}
-              </Link>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="border-y-8 border-y-olive">
+      <div className="font-bold italic">Site Map</div>
+      {mapLinks.map((page, index) => (
+        <div key={page} className="py-1 px-4">
+          <div className="text-center">
+            <Link to={linkPaths[index]}>
+              {page}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
