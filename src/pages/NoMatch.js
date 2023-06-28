9c0a9dd8269b07e7f944382435b2111dc6ec5c48
TheClutchC <claycarney7@gmail.com>
2023-06-28 10:41:21 -0400

    add link for HomePage when route doesn't exist

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const NoMatch = () => {
   return (
     <div style={{ padding: 20 }}>
       <h2>404: Page Not Found</h2>
       <p>This page doesn't exist yet.</p>
+      <Link to="/" style={{ textDecoration: "underline", color: "blue" }}>Go to the home page</Link>
     </div>
   );
 }
