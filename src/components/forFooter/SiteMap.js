ec03cec4548d9b5d0f5bb61295f9f4f065a6085d
TheClutchC <claycarney7@gmail.com>
2023-07-07 15:10:06 -0400

    add icons to footer/ active links red

--- src/components/forFooter/SiteMap.js
+++ src/components/forFooter/SiteMap.js
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { GrHome, GrCircleInformation, GrServices, GrContactInfo   } from "react-icons/gr";
 
 const mapLinks = [
-  'Home', 
-  'About', 
-  'Services', 
-  'Contact'
+  { title: 'Home', icon: GrHome },
+  { title: 'About', icon: GrCircleInformation },
+  { title: 'Services', icon: GrServices },
+  { title: 'Contact', icon: GrContactInfo }
 ];
 
 const SiteMap = () => {
@@ -12,13 +13,14 @@ const SiteMap = () => {
 
   return (
     <div>
-      <div className="font-bold italic">Site Map</div>
+      <div className="text-center font-bold italic">Site Map</div>
       {mapLinks.map((page, index) => (
-        <div key={page} className="py-1 px-4">
-          <div className="text-center">
-            <Link to={linkPaths[index]}>
-              {page}
-            </Link>
+        <div key={page.title} className="inline-flex py-2 px-4">
+          <div>
+            <NavLink className="sitemap" to={linkPaths[index]}>
+              <page.icon className="text-center text-2xl m-auto"/> 
+              <span>{page.title}</span>
+            </NavLink>
           </div>
         </div>
       ))}
