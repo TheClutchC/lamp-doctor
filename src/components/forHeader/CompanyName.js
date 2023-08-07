48285ee6bf190f8e39d6a25931d946f556c3ca83
TheClutchC <claycarney7@gmail.com>
2023-08-07 02:33:03 -0400

    revert company name color scheme

--- src/components/forHeader/CompanyName.js
+++ src/components/forHeader/CompanyName.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const CompanyName = () => {
   return (
-    <Link to="/" className="flex font-extrabold px-4 py-2 rounded-xl bg-yellowesque">
+    <Link to="/" className="flex font-extrabold px-4 py-2 rounded-xl bg-platinum bg-opacity-50">
       Lamp Doctor of Santa Fe
     </Link>
   );
