191b28ba1527ede740e8d24eee76fcd99bfc239f
TheClutchC <claycarney7@gmail.com>
2024-04-14 09:21:39 -0400

    update from HashRouter to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
